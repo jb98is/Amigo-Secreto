e39e880edb61e511c50f8f40243d3db5ee74b2d8
jb <@jb8is>
2025-09-05 00:24:48 -0400

    Actualiza1

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+  let listaAmigos =[];
+function agregarAmigo(){
+ 
+  let amigo = document.querySelector("#amigo").value;
+  let c=0;
+  
+  function mostrarP(texto,m) {
+    let text = document.querySelector(m);
+    text.innerHTML =texto;
+  }
+  function guardarAmigos(amigo){
+    listaAmigos.push(amigo);
+    c++;
+    mostrarP(listaAmigos,"p");
+  }
+  
+  guardarAmigos(amigo);
+}
